8921fb2295f85253de88828d508bd9b398d7a6d6
Zachary Ginis <61295579+zacharyginis@users.noreply.github.com>
2025-12-14 14:01:14 -0600

    Fix Vite build config and add autocomplete

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     port: 8080
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
   }
 })
